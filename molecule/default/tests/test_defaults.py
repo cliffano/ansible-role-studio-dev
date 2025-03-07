ba0b2abf861cb6a3b107dfdf359d9915ea5baa8d
Cliffano Subagio <cliffano@gmail.com>
2025-03-07 23:43:35 +1100

    Add gpg agent file test.

--- molecule/default/tests/test_defaults.py
+++ molecule/default/tests/test_defaults.py
@@ -2,8 +2,19 @@ import pytest
 
 def test_git_aliases(host):
 
-    content_file = host.file("/tmp/.oh-my-zsh/custom/aliases-git.zsh")
-    assert content_file.exists
-    assert content_file.is_file
-    assert content_file.mode == 0o644
-    assert "alias gaa=git add -A" in content_file.content_string
+    git_aliases_file = host.file("/tmp/.oh-my-zsh/custom/aliases-git.zsh")
+    assert git_aliases_file.exists
+    assert git_aliases_file.is_file
+    assert git_aliases_file.mode == 0o644
+    assert "alias gaa=git add -A" in git_aliases_file.content_string
+    assert "alias gta=git tag -a" in git_aliases_file.content_string
+
+def test_gpg_agent_conf(host):
+
+    gpg_agent_file = host.file("/tmp/.gnupg/gpg-agent.conf")
+    assert gpg_agent_file.exists
+    assert gpg_agent_file.is_file
+    assert gpg_agent_file.mode == 0o644
+    assert "pinentry-program /usr/local/bin/pinentry-mac" in gpg_agent_file.content_string
+    assert "default-cache-ttl" in gpg_agent_file.content_string
+    assert "max-cache-ttl 28800" in gpg_agent_file.content_string
