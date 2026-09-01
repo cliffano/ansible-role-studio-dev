8b3af007421396a92615d389b222c11bf85cdcae
Cliffano Subagio <cliffano@gmail.com>
2026-09-01 21:52:58 +1000

    Increase cpu allocation to 4.

--- molecule/default/tests/test_defaults.py
+++ molecule/default/tests/test_defaults.py
@@ -150,4 +150,4 @@ def test_docker_aliases(host):
 def test_colima_aliases(host):
 
     profile_file = host.file("/tmp/.zprofile")
-    assert "alias co5-studio='sudo rm -f /var/run/docker.sock && sudo ln -s /tmp/.colima/studio/docker.sock /var/run/docker.sock && colima start --arch x86_64 --cpu-type \"max,+avx,+avx2\" --cpu 2 --memory 4 studio'" in profile_file.content_string
+    assert "alias co5-studio='sudo rm -f /var/run/docker.sock && sudo ln -s /tmp/.colima/studio/docker.sock /var/run/docker.sock && colima start --arch x86_64 --cpu-type \"max,+avx,+avx2\" --cpu 4 --memory 4 studio'" in profile_file.content_string
